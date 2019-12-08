81f69bf9471e76ffefc3378cd9c866c3ce6ae077
Alex Soin <3787132+alexsoin@users.noreply.github.com>
2019-12-08 23:19:28 +0400

    правка подключения bootstrap

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,8 +4,8 @@ import 'jquery';
 // Импортируем Popper
 import 'popper.js';
 
-// Импортируем необходимые js-файлы Bootstrap 4
-import 'bootstrap/js/dist/util';
+// Импортируем Bootstrap 4
+import 'bootstrap';
 
 // Импортируем другие js-файлы
 import './_my';
