bba9cc400cff95ac802b56421842d0a98138bc32
Alex Soin <alex94soin@gmail.com>
2021-12-09 16:26:09 +0400

    feat: images
    Отключение сжатия изображений для dev режима

--- src/configs/tasks/images.js
+++ src/configs/tasks/images.js
@@ -1,13 +1,17 @@
 import gulp from "gulp";
-import cache from "gulp-cache";
 import image from "gulp-image";
+import gulpif from "gulp-if";
+import yargs from "yargs";
 
 import paths from "../paths.config.js";
 
+const { argv } = yargs;
+const developer = !!argv.developer;
+const production = !developer;
 
 /** Обработка картинок */
 export default function images() {
 	return gulp.src(paths.src.img)
-		.pipe(cache(image()))
+		.pipe(gulpif(production, image()))
 		.pipe(gulp.dest(paths.dist.img));
 }
